29e170618ae9f08a6da9a0144660ea5fe878962f
jmenne <jo.menne@arcor.de>
2020-09-25 11:51:31 +0200

    Create InstallChocoPackages.ps1

---
+++ InstallChocoPackages.ps1
@@ -0,0 +1,17 @@
+﻿#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Assign Packages to Install
+$Packages = 'vscode',`
+            'azure-cli',`
+            'foxitreader' ,`
+            'microsoftazurestorageexplorer',`
+            'sql-server-management-studio'
+          
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+#Reboot
+Restart-Computer
